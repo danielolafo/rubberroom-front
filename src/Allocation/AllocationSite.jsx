be949912947b6158f52bc4b1dc3f63c91889111a
danielolafo <danielolafo@gmail.com>
2025-09-11 16:13:17 -0500

    Added allocation detail info component

--- src/Allocation/AllocationSite.jsx
+++ src/Allocation/AllocationSite.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 function AllocationSite(allocationId){
-
+    const base_path = process.env.BASE_PATH;
     const [allocation, setAllocation] = useState({
         id:   '',
         city:  '',
@@ -35,6 +35,70 @@ function AllocationSite(allocationId){
         <div>
             <input type="text" disabled value={allocation.address} />
         </div>
+        </>
+    );
+}
+
+function AllocationInfo(){
+    const [allocation, setAllocation] = useState({
+        id:   '',
+        city:  '',
+        address: ''
+    });
+    const location = useLocation();
+        const {id} = location.state;
+
+    const getAllocationDetail = async () => {
+        axios.get("http://localhost:8000/allocation/"+id, [])
+            .then((response) => {
+                setAllocation(response.data);
+            }).catch(error => {
+                setAllocation(null);
+            });
+    }
+
+    useEffect(() => {
+        getAllocationDetail();
+    }, []);
+
+    return (
+        <>
+        <div>
+            <input type="text" disabled value={allocation.city}/>
+        </div>
+        <div>
+            <input type="text" disabled value={allocation.address} />
+        </div>
+        </>
+    );
+}
+
+function AllocationMedia(){
+
+    const [allocation, setAllocation] = useState({
+        id:   '',
+        city:  '',
+        address: ''
+    });
+
+    const getAllocationMedia = async () => {
+        axios.get("http://localhost:8000/media-data/"+0, [])
+            .then((response) => {
+                setAllocation(response.data);
+            }).catch(error => {
+                setAllocation(null);
+            });
+    }
+
+    useEffect(() => {
+        getAllocationMedia();
+    }, []);
+
+    return(
+        <>
+            <div>
+                
+            </div>
         </>
     );
 }
